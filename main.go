548a0d66f7481099a748d9a2b6fedd1130de22fb
ggarber42 <ggarber@protonmail.com>
2023-09-12 21:24:24 -0300

    feat(wip): add gin server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
-	fmt.Println("Hello Crud")
+	server := gin.Default()
+	server.GET("/hello", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "go crud api",
+		});
+	})
+	server.Run("localhost:5000");
+	fmt.Printf("go crud log")
 }
