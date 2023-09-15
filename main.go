4bb14f26dd5ff741ad64cac32b304e40e3f3b4d2
ggarber42 <ggarber@protonmail.com>
2023-09-14 23:06:05 -0300

    feat: add todo controler to main

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
+
+	"github.com/ggarber42/go-crud-api/controllers"
+	"github.com/ggarber42/go-crud-api/initializers"
 )
+
+func init(){
+	initializers.ConnectDB()
+}
 
 func main(){
 	server := gin.Default()
+
 	server.GET("/hello", func(c *gin.Context){
 		c.JSON(200, gin.H{
 			"message": "go crud api",
 		});
 	})
+
+	server.POST("/todos", controllers.CreateTodo)
+
+
+
 	server.Run("localhost:5000");
 	fmt.Printf("go crud log")
 }
